4811d1d7eec8a731f5f7f8f552104165de209860
mdyasin7636 <mdyasin7636@gmail.com>
2024-06-12 14:35:17 +0600

    project added

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <button className="btn btn-outline">Default</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0eZln-6SZpkbh00plzvcLTlkBQdXnls4",
+  authDomain: "eduhub-da720.firebaseapp.com",
+  projectId: "eduhub-da720",
+  storageBucket: "eduhub-da720.appspot.com",
+  messagingSenderId: "247380618899",
+  appId: "1:247380618899:web:94ea533a5d403bee43a766",
+};
+
+export const app = initializeApp(firebaseConfig);
